379507a9c60ba1d6a565d3fdc0097cc06fd4ea48
Gabriel H. Finotti <finotti.dev@gmail.com>
2025-08-24 23:30:01 -0300

    Add teachers dashboard UI and styles
    Introduces a new SCSS module for styling the teachers dashboard and updates the page to include header, filters, and a user table. Enhances the interface for managing and adding teachers with improved layout and components.

--- src/app/dashboard/teachers/page.tsx
+++ src/app/dashboard/teachers/page.tsx
@@ -1,8 +1,29 @@
+import UserTable from "@/components/layout/tables/userTable/UserTable";
+import AddContentButton from "@/components/ui/buttons/addContentButton/AddContentButton";
+import TableFilter from "@/components/ui/buttons/tableFilter/TableFilter";
+import SearchInput from "@/components/ui/inputs/searchInput/SearchInput";
+import style from "./Teachers.module.scss";
+
 const TeachersPage = () => {
   return (
-    <div>
-      <h1>Professores</h1>
-    </div>
+    <>
+      <header className={style.header}>
+        <section className={style.headerTitle}>
+          <div>
+            <h1>Painel de Professores</h1>
+            <p>Gerenciar ou Adicionar Professores!</p>
+          </div>
+          <AddContentButton label="Adicionar Professor" />
+        </section>
+        <section className={style.headerFilters}>
+          <SearchInput placeholder="Buscar Professores" />
+          <TableFilter />
+        </section>
+      </header>
+      <main className={style.main}>
+        <UserTable />
+      </main>
+    </>
   );
 };
 

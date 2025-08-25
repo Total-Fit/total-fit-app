df42e04d7dada7c11d0314d1fbe16276832dcf04
Gabriel H. Finotti <finotti.dev@gmail.com>
2025-08-25 04:05:21 -0300

    Add RegisterForm component and styles
    Introduces a new RegisterForm React component with associated SCSS module for layout and styling. The form includes multiple input fields, a close button, and a submit button, designed for user registration.

---
+++ src/components/layout/forms/registerForm/RegisterForm.tsx
@@ -0,0 +1,55 @@
+import Input from "@/components/ui/inputs/defaultsFields/Input";
+import style from "./RegisterForm.module.scss";
+import SubmitInput from "@/components/ui/inputs/submitInput/SubmitInput";
+import Image from "next/image";
+
+type RegisterFormProps = {
+  formTitle: string;
+  onClose: () => void;
+};
+
+const RegisterForm = ({ formTitle, onClose }: RegisterFormProps) => {
+  const fields = [
+    "Nome",
+    "Contato",
+    "Email",
+    "CPF",
+    "RG",
+    "Endereço",
+    "Peso",
+    "Altura",
+    "Objetivo",
+    "Instagram",
+    "Nome de Usuário",
+    "Senha",
+    "Data de Pagamento",
+  ];
+
+  return (
+    <div className={style.container}>
+      <form className={style.form}>
+        <button type="button" className={style.closeButton} onClick={onClose}>
+          <Image
+            src="/close_icon.svg"
+            alt="Image description"
+            width={38}
+            height={38}
+          />
+        </button>
+        <h1>{formTitle}</h1>
+        <div className={style.fields}>
+          {fields.map((field) => (
+            <div key={field}>
+              <Input isAuthForm label={field} id={field} />
+            </div>
+          ))}
+          <div className={style.submit}>
+            <SubmitInput />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default RegisterForm;

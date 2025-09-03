a89de26d2d42fc5ea67035e860441f55176c03ad
Gabriel H. Finotti <finotti.dev@gmail.com>
2025-09-03 18:50:07 -0300

    Add dashboard redirect on auth form submit
    Introduced a handleSubmit function to the AuthForm component that prevents default form submission and redirects the user to the /dashboard route using Next.js useRouter.

--- src/components/layout/forms/authForm/AuthForm.tsx
+++ src/components/layout/forms/authForm/AuthForm.tsx
@@ -3,15 +3,25 @@
 import Input from "@/components/ui/inputs/defaultsFields/Input";
 import style from "./AuthForm.module.scss";
 import SubmitInput from "@/components/ui/inputs/submitInput/SubmitInput";
+import React from "react";
+import { useRouter } from "next/navigation";
 
 const AuthForm = () => {
+  const route = useRouter();
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    route.push("/dashboard");
+  };
+
   return (
     <div className={style.formContainer}>
       <div className={style.formHeader}>
         <h1>Área de Acesso</h1>
         <h3>Dashboard administrativo Total Fit Academia</h3>
       </div>
-      <form className={style.form}>
+      <form className={style.form} onSubmit={handleSubmit}>
         <Input
           isAuthForm
           label="Usuário"
